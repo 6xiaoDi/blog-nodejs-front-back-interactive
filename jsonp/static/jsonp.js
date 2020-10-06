0722b9f330e9b2fd7fea8485ea85344021d070fd
6xd <ldtheended@163.com>
2020-10-06 12:28:27 +0800

    a0.33（请求百度jsonp接口）

---
+++ jsonp/static/jsonp.js
@@ -0,0 +1,82 @@
+function ajax(options) {
+    let opts = Object.assign({
+        method: 'get',
+        url: '',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        jsonp:"cb", // 默认cb
+        data: '',
+        success: function () { }
+    }, options)
+    //处理jsonp请求；
+    if(opts.dataType==="jsonp"){
+        // 跨域请求
+        // url、数据、回调名、执行的回调函数
+        jsonpFn(opts.url,opts.data,opts.jsonp,opts.success);
+        return false;
+    }
+    
+    function jsonpFn(url,data,cbName,cbFn){
+        // cbName：cb/callback
+        // 回调函数名
+        let fnName = "ME_"+Math.random().toString().substr(2); // 随机函数名（去掉0.）
+        window[fnName] = cbFn; //函数挂在window上
+        // 请求路径
+        // 最后不能直接加cbfn，会直接把整个函数赋值上去的，而我们仅需要函数名  =>  fnName
+        let path = url+"?"+o2u(data)+"&"+cbName+"="+fnName;
+        // console.log(path);
+        let o = document.createElement("script");
+        o.src = path;
+        document.querySelector("head").appendChild(o);
+    }
+
+
+
+    let xhr = new XMLHttpRequest();
+    
+    if (options.method == "get") {
+        let data = o2u(opts.data)
+        options.url = options.url + "?" + data;
+    }
+    
+    xhr.open(options.method, options.url, true);
+    
+    for (let key in opts.headers) {
+        xhr.setRequestHeader(key, opts.headers[key]);
+    }
+    
+    let sendData;
+    switch (opts.headers['content-type']) {
+        case 'application/x-www-form-urlencoded':
+            sendData = o2u(opts.data);
+            break;
+        case 'application/json':
+            sendData = JSON.stringify(opts.data);
+            break;
+    }
+    
+    xhr.onload = function () {
+        let resData;
+        if (xhr.getResponseHeader("content-type").includes("xml")) {
+            resData = xhr.responseXML;
+        } else {
+            resData = JSON.parse(xhr.responseText);
+        }
+        options.success(resData);
+    }
+    if (options.method == "get") {
+        xhr.send();
+    } else {
+        xhr.send(sendData);
+    }
+}
+
+// 将对象（单层遍历）转为queryString的形式
+function o2u(obj) {
+    let keys = Object.keys(obj);
+    let values = Object.values(obj);
+    return keys.map((v, index) => {
+        return `${v}=${values[index]}`;
+    }).join("&");
+}
