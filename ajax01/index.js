c12749af5138a78bc36e5fb0b5ecf51bcf32df85
6xd <ldtheended@163.com>
2020-10-11 09:19:44 +0800

    a0.41（ajax使用——xml数据返还，未设置头部）

--- ajax01/index.js
+++ ajax01/index.js
@@ -13,7 +13,21 @@ router.get("/",ctx=>{
     ctx.body = "some value..."
 })
 router.get("/xml",ctx=>{
-    ctx.body = "some xml value...";
+    // ctx.body = "some xml value..."
+    // 设置头部
+    // ctx.set("content-type","text/xml");
+    ctx.body = `<?xml version='1.0' encoding='utf8' ?>
+                    <books>
+                        <nodejs>
+                            <name>nodjs</name>
+                            <price>$5.7</price>
+                        </nodejs>
+                        <vuejs>
+                            <name>vue从入门到精通</name>
+                            <price>$5.8</price>
+                        </vuejs>
+                    </books>
+                `
 })
 app.use(router.routes());
 app.listen(4000);
