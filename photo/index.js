f64f335ff89f20da3503557aaacfcc23b533cd05
6xd <ldtheended@163.com>
2020-10-11 23:37:52 +0800

    a0.51（通过formData实现《qq空间批量上传图片》—— 基本实现所有功能）

--- photo/index.js
+++ photo/index.js
@@ -4,6 +4,10 @@ const static = require("koa-static");
 const Router = require("koa-router");
 // 接收post参数及上传的文件；
 const koaBody = require("koa-body");
+
+const users = require("./data/users.json");
+const images = require("./data/images.json");
+
 let app = new Koa();
 let router = new Router();
 app.use(static(__dirname+"/static"));
@@ -16,18 +20,57 @@ router.get("/",ctx=>{
 
 // 上传接口
 router.post("/upload",ctx=>{
-    let filepath = ctx.request.files.img.path;
-    // 判断是否有下载目录，否则创建目录
-    if(!fs.existsSync("static/uploads/")){
-        fs.mkdirSync("static/uploads/");
+    fs.createReadStream(ctx.request.files['img'].path).pipe(fs.createWriteStream("static/imgs/" +      ctx.request.files['img'].name))
+    //将图片路径存入images.json文件
+    let uid = ctx.cookies.get("uid");
+    let imageData = {
+        imgUrl:`imgs/${ctx.request.files['img'].name}`,
+        imgName:ctx.request.files['img'].name,
+        uid
     }
-    // 流方式转存 ； 文件路径；
-    let rs =  fs.createReadStream(filepath);
-    rs.pipe(fs.createWriteStream("static/uploads/"+ctx.request.files.img.name));
-    ctx.body ={
-        info:"转存成功",
-        status:1
+    images.push(imageData);
+    let res = fs.writeFileSync("data/images.json",JSON.stringify(images));
+    ctx.body = res;
+
+})
+
+router.get("/checkUserName", (ctx, next) => {
+    let res = users.find(v => v.username == ctx.query.name);
+    if (res) {
+        ctx.body = {
+            status: 1,
+            info: "用户名正确"
+        }
+    } else {
+        ctx.body = {
+            status: 2,
+            info: "用户名错误"
+        }
     }
 })
+
+// 用户登录验证
+router.post("/checkUser",(ctx,next)=>{
+    let res =  users.find(v=>(v.username==ctx.request.body.username && v.pwd==ctx.request.body.pwd));
+    if(res){
+        ctx.cookies.set("uid",res.uid,{
+            maxAge:30*24*60*60*1000,
+            overwrite:true,
+            httpOnly:true,
+        })
+        ctx.redirect("/photo.html");
+    }else{
+        ctx.redirect("/login.html");
+    }
+})
+
+// 查询相册数据做对应呈现；
+router.get("/getImageData",(ctx,next)=>{
+    let uid = ctx.cookies.get("uid");
+    // 查询当前用户上传的所有图片（通过uid进行用户区分）
+    let res =  images.filter(v=>v.uid==uid);
+    ctx.body = res;
+})
+
 app.use(router.routes());
 app.listen(3000);
