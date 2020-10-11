b1131e9eaa04e3047d41b0cbec33e1c8bd642dae
6xd <ldtheended@163.com>
2020-10-11 21:39:11 +0800

    a0.49（通过formData实现《qq空间批量上传图片》—— 实现后台上传功能）

--- photo/index.js
+++ photo/index.js
@@ -1,4 +1,5 @@
 const Koa = require("koa");
+const fs = require("fs");
 const static = require("koa-static");
 const Router = require("koa-router");
 // 接收post参数及上传的文件；
@@ -11,6 +12,22 @@ app.use(koaBody({
 }))
 router.get("/",ctx=>{
     ctx.body = "some value";
+})
+
+// 上传接口
+router.post("/upload",ctx=>{
+    let filepath = ctx.request.files.img.path;
+    // 判断是否有下载目录，否则创建目录
+    if(!fs.existsSync("static/uploads/")){
+        fs.mkdirSync("static/uploads/");
+    }
+    // 流方式转存 ； 文件路径；
+    let rs =  fs.createReadStream(filepath);
+    rs.pipe(fs.createWriteStream("static/uploads/"+ctx.request.files.img.name));
+    ctx.body ={
+        info:"转存成功",
+        status:1
+    }
 })
 app.use(router.routes());
 app.listen(3000);
