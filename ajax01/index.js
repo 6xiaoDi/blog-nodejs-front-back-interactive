a6d7de98bacb240fd5ec457758631a01c620ee68
6xd <ldtheended@163.com>
2020-10-11 09:06:32 +0800

    a0.39（ajax使用——使用static中间件加载视图）

---
+++ ajax01/index.js
@@ -0,0 +1,16 @@
+const Koa = require("koa");
+const static = require("koa-static");
+const Router = require("koa-router");
+// 接收post参数及上传的文件；
+const koaBody = require("koa-body");
+let app = new Koa();
+let router = new Router();
+app.use(static(__dirname+"/static"));
+app.use(koaBody({
+    multipart:true
+}))
+router.get("/",ctx=>{
+    ctx.body = "some value..."
+})
+app.use(router.routes());
+app.listen(4000);
