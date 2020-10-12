679375cefa8477a2658e5b669dbbf6d8814fe357
6xd <ldtheended@163.com>
2020-10-12 13:17:09 +0800

    a0.53（请求非同源）

---
+++ ajax02/http4000/index.js
@@ -0,0 +1,17 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const static = require("koa-static");
+let app = new Koa();
+let router = new Router();
+app.use(static(__dirname+"/static"));
+router.get("/",ctx=>{
+    ctx.body = "some value... at 4000";
+});
+router.post("/getData",ctx=>{
+    console.log("请求过来了");
+    ctx.body = {
+        info:"I am at 4000"
+    };
+});
+app.use(router.routes());
+app.listen(4000);
