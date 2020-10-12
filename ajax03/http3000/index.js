57464e7f223d890d3c4d1d6e750681a403f27726
6xd <ldtheended@163.com>
2020-10-12 17:42:06 +0800

    a0.71（请求非同源——后端代理——利用 koa-server-http-proxy中间件实现代理）

--- ajax03/http3000/index.js
+++ ajax03/http3000/index.js
@@ -1,9 +1,14 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const static = require("koa-static");
-const axios = require("axios");
+// const axios = require("axios");
+// 服务器代理 ：一次性所有接口都代理
+const koaServerHttpProxy = require("koa-server-http-proxy");
 let app = new Koa();
 let router = new Router();
+
+app.use(koaServerHttpProxy('/api', { target: 'http://localhost:4000', pathRewrite: { '^/api': '' } }))
+
 app.use(static(__dirname + "/static"));
 
 router.get("/", ctx => {
@@ -11,14 +16,14 @@ router.get("/", ctx => {
 });
 
 // 代理转发；
-router.post("/getData", async ctx => {
-    let res = await axios({
-        method: "post",
-        url: "http://localhost:4000/getDataService"
-    })
-    console.log(res.data);
-    ctx.body = res.data;
-})
+// router.post("/getData", async ctx => {
+//     let res = await axios({
+//         method: "post",
+//         url: "http://localhost:4000/getDataService"
+//     })
+//     console.log(res.data);
+//     ctx.body = res.data;
+// })
 
 app.use(router.routes());
 app.listen(3000);
