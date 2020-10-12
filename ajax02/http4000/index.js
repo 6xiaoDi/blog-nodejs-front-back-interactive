2bed614709e9755ab2ef3d7c07744186e8048a29
6xd <ldtheended@163.com>
2020-10-12 15:54:55 +0800

    a0.60（请求非同源——预检请求通过后仍报错——koa-router 7.4 版本）

--- ajax02/http4000/index.js
+++ ajax02/http4000/index.js
@@ -4,9 +4,17 @@ const static = require("koa-static");
 let app = new Koa();
 let router = new Router();
 app.use(static(__dirname+"/static"));
+// 对所有的预检请求都可以通过（给任何返回，它就默认是可以通过的）
+router.options("/*",ctx=>{
+    // 允许cors跨域；
+    ctx.set("Access-Control-Allow-Origin","http://localhost:3000")
+    ctx.body = "";
+})
+
 router.get("/",ctx=>{
     ctx.body = "some value... at 4000";
 });
+
 router.post("/getData",ctx=>{
     console.log("请求过来了");
     // 允许跨域；允许地址
@@ -16,5 +24,6 @@ router.post("/getData",ctx=>{
         info:"I am at 4000"
     };
 });
+app.use(router.allowedMethods());
 app.use(router.routes());
 app.listen(4000);
