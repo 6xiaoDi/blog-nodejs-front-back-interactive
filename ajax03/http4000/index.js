be6708b84cd2327fb3996b7b92329a006fc4b20c
6xd <ldtheended@163.com>
2020-10-12 17:23:40 +0800

    搭建代理服务

--- ajax03/http4000/index.js
+++ ajax03/http4000/index.js
@@ -4,45 +4,9 @@ const static = require("koa-static");
 let app = new Koa();
 let router = new Router();
 app.use(static(__dirname+"/static"));
-// 对所有的预检请求都可以通过（给任何返回，它就默认是可以通过的）
-router.options("/:splat*",ctx=>{
-    // 允许cors跨域；
-    ctx.set("Access-Control-Allow-Origin","http://localhost:3000")
-    // 允许设置的头部信息；
-    // 注意与正式请求一致
-    ctx.set("Access-Control-Allow-Headers","Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild")
-    // 允许前端获取的头部
-    // 如果是多个可在第二个参数中用逗号隔开
-    ctx.set("Access-Control-Expose-Headers","Date,Access-Control-Allow-Headers")
-    //允许前端请求的方法 ；
-    ctx.set("Access-Control-Allow-Methods",'PUT, POST, GET, DELETE, OPTIONS');
-    // 允许携带凭证；cookie  --》允许跨域的地址需要指定 ："http://localhost:3000"
-    ctx.set("Access-Control-Allow-Credentials",true);
-    ctx.body = "";
-})
 
 router.get("/",ctx=>{
     ctx.body = "some value... at 4000";
-});
-
-router.post("/getData",ctx=>{
-    console.log("请求过来了");
-    // 允许跨域；允许地址
-    // ctx.set("Access-Control-Allow-Origin","*")
-    ctx.set("Access-Control-Allow-Origin","http://localhost:3000")
-    // 允许设置的头部信息；（如果允许还有多个的话，可继往后追加），除此之外的头部信息，是不允许的，前端需提前与后端商量
-    // 注意预检请求也得同步设置
-    ctx.set("Access-Control-Allow-Headers","Content-type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild")
-    // 允许前端获取的头部
-    // 如果是多个可在第二个参数中用逗号隔开
-    ctx.set("Access-Control-Expose-Headers","Date,Access-Control-Allow-Headers")
-    //允许前端请求的方法 ；
-    ctx.set("Access-Control-Allow-Methods",'PUT, POST, GET, DELETE, OPTIONS');
-    // 允许携带凭证；cookie  --》允许跨域的地址需要指定 ："http://localhost:3000"
-    ctx.set("Access-Control-Allow-Credentials",true);
-    ctx.body = {
-        info:"I am at 4000"
-    };
 });
 
 app.use(router.routes());
