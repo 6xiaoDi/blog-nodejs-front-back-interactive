c1aec375ddc0a6506f4f014b769e935584cb0f49
6xd <ldtheended@163.com>
2020-10-13 10:18:32 +0800

    a0.81（axios使用——函数式调用发送get请求）

---
+++ axios/index.js
@@ -0,0 +1,20 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const koaBody = require("koa-body");
+const static = require("koa-static");
+let app = new Koa();
+let router = new Router();
+app.use(koaBody());
+app.use(static(__dirname+"/static"));
+router.get("/", ctx=>{
+    ctx.body = "some value......";
+})
+
+router.get("/axios",ctx=>{
+    console.log("axios get请求过来");
+    ctx.body = {
+        info:"get返还数据"
+    }
+})
+app.use(router.routes());
+app.listen(4000);
