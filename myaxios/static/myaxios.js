21f79b73b1f190af5418dfb84949c2c3bd66bfc0
6xd <ldtheended@163.com>
2020-10-14 14:12:34 +0800

    a0.96（实现简易axios——将Axios原型上的方法混入到request方法上，并保护this，将Axios构造函数中属性绑定在request方法上）

--- myaxios/static/myaxios.js
+++ myaxios/static/myaxios.js
@@ -1,11 +1,14 @@
 let utils = {
-    // a函数(但是把其当做对象来用)      b原型
-    extends(a,b){
+    // a函数(但是把其当做对象来用)      b原型     context 保护this执行
+    extends(a,b,context){
         for(let key in b){
             // for in 会循环原型链里的内容，因此最好判断一下原型链上有没有key，要是原型链有其他东西我们不去管它（不进逻辑）
             if(b.hasOwnProperty(key)){
                 if(typeof b[key]==='function'){
                     // 函数；
+                    a[key] = b[key].bind(context);
+                }else{
+                    // 属性；
                     a[key] = b[key]
                 }
             }
@@ -15,6 +18,7 @@ let utils = {
 
 class Axios{
     constructor(){
+        this.test = "一些属性";
     }
     request(config){
         return new Promise(resolve=>{
@@ -37,7 +41,9 @@ function createInstance(){
     let context = new Axios();
     let instance = context.request;
     // 把原型里的方法混入到instance里；
-    utils.extends(instance,Axios.prototype);
+    utils.extends(instance,Axios.prototype,context);
+    utils.extends(instance,context); // 将构造函数的属性混入进去
+    console.dir(instance);
     return instance
 }
 let axios = createInstance();
