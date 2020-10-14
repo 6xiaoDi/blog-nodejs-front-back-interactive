745c274b9d694e7088cfee0344561d446ed7c5a5
6xd <ldtheended@163.com>
2020-10-14 14:02:37 +0800

    a0.94（实现简易axios——将Axios原型上的方法混入到request方法上）

--- myaxios/static/myaxios.js
+++ myaxios/static/myaxios.js
@@ -1,3 +1,18 @@
+let utils = {
+    // a函数(但是把其当做对象来用)      b原型
+    extends(a,b){
+        for(let key in b){
+            // for in 会循环原型链里的内容，因此最好判断一下原型链上有没有key，要是原型链有其他东西我们不去管它（不进逻辑）
+            if(b.hasOwnProperty(key)){
+                if(typeof b[key]==='function'){
+                    // 函数；
+                    a[key] = b[key]
+                }
+            }
+        }
+    }
+}
+
 class Axios{
     constructor(){
     }
@@ -16,12 +31,14 @@ methodsArr.forEach(method=>{
         return this.request(config);
     }
 })
-console.dir(Axios);
 
 // 创建实例
 function createInstance(){
     let context = new Axios();
     let instance = context.request;
+    // 把原型里的方法混入到instance里；
+    utils.extends(instance,Axios.prototype);
+    console.dir(instance);
     return instance
 }
 let axios = createInstance();
